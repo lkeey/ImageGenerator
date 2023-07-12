022d73cbf57fe55ff659bcc915acc416c0caf586
lkeey <leon062007@mail.ru>
2023-07-12 19:11:42 +0300

    create backend; get images from open ai

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3001;
 const app = express();
 
-app.route('/opanai', require('./routes/openaiRoutes'));
+app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
